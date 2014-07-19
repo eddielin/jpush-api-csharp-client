df98e364da12321a7586ba6236adbca985eea07c
eddielin <linqr@jpush.cn>
2014-07-19 18:09:56 +0800

    1.init folder structs 2.add text resources

---
+++ JPushApi/Push/PushClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPush.Api.Push
+{
+    class PushClient
+    {
+        private string appKey = null;
+        private string masterSecret = null;
+        private bool isEnableSSL = false;
+        private bool isApnsProduction = false;
+
+        private long timeToLive = 86400;
+    }
+}
